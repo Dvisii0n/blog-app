accd9fdc0d280a3dbcc6f6ecc76512108b15e49c
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-04-20 20:27:51 -0600

    defined variable types

--- backend/app.ts
+++ backend/app.ts
@@ -1,25 +1,31 @@
-import express from 'express'
-import passport from 'passport'
-import postRouter from './routes/postRouter'
-import authRouter from './routes/authRouter'
-import cors from 'cors'
-import { default404Handler, default500Handler } from './middleware/errorHandlers'
-import strategyJWT from './config/passport'
+import express from "express";
+import type { Application } from "express";
+import passport from "passport";
+import postsRouter from "./routes/postsRouter";
+import authRouter from "./routes/authRouter";
+import cors from "cors";
+import {
+	default404Handler,
+	default500Handler,
+} from "./middleware/errorHandlers";
+import strategyJWT from "./config/passport";
 
-const app = express()
-const PORT = 3000
+const PORT: string = process.env.PORT as string;
 
-app.use(cors())
-app.use(express.urlencoded())
-passport.use(strategyJWT)
+const app: Application = express();
+const baseRoute: string = "/api/v1";
 
-app.use('/api/auth', authRouter)
-app.use('/api/posts', postRouter)
+app.use(cors());
+app.use(express.urlencoded());
+passport.use(strategyJWT);
 
-app.use(default404Handler)
-app.use(default500Handler)
+app.use(`${baseRoute}/auth`, authRouter);
+app.use(`${baseRoute}/posts`, postsRouter);
 
-app.listen(PORT || 3000, (err) => {
-    if (err) throw err
-    console.log(`Server running on port ${PORT}`)
-})
+app.use(default404Handler);
+app.use(default500Handler);
+
+app.listen(PORT, (err) => {
+	if (err) throw err;
+	console.log(`Server running on port ${PORT}`);
+});

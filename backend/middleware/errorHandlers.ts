15c23b74a807b3eaa5df33a12711908e5f6e9f75
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-04-21 20:36:37 -0600

    code reformat

--- backend/middleware/errorHandlers.ts
+++ backend/middleware/errorHandlers.ts
@@ -1,12 +1,17 @@
-import type { NextFunction, Request, Response } from 'express'
+import type { NextFunction, Request, Response } from "express";
 
-async function default404Handler(req: Request, res: Response)  {
-    res.status(404).send('404 Not Found')
+async function default404Handler(req: Request, res: Response) {
+	res.status(404).send("404 Not Found");
 }
 
-async function default500Handler(err: Error, req: Request, res: Response, next:NextFunction) {
-    console.error(err)
-    res.status(500).send('500 Server Error')
+async function default500Handler(
+	err: Error,
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
+	console.error(err);
+	res.status(500).send("500 Server error");
 }
 
-export {default404Handler, default500Handler}
+export { default404Handler, default500Handler };

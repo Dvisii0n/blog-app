c5b2b1fac7f41f234660c200612dc6be4c5eb7ce
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-04-25 15:36:43 -0600

    code reformat

--- backend/config/passport.ts
+++ backend/config/passport.ts
@@ -1,24 +1,24 @@
-import type { StrategyOptions } from 'passport-jwt'
-import {Strategy as JwtStrategy, ExtractJwt} from 'passport-jwt'
-import { prisma } from '../lib/prisma'
+import type { StrategyOptions } from "passport-jwt";
+import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
+import { prisma } from "../lib/prisma";
 
 const opts: StrategyOptions = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: `${process.env.JWT_SECRET}`
-}
+	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+	secretOrKey: `${process.env.JWT_SECRET}`,
+};
 
 const strategyJWT = new JwtStrategy(opts, async (jwt_payload, done) => {
-    try {
-        const userId = jwt_payload.user.id
-        const user = await prisma.user.findUnique({where: {id: userId}})
-        if (user) {
-            return done(null, user)
-        }        
+	try {
+		const userId = jwt_payload.user.id;
+		const user = await prisma.user.findUnique({ where: { id: userId } });
+		if (user) {
+			return done(null, user);
+		}
 
-        return done(null, false)
-    } catch (err) {
-        return done(err)
-    }
-})
+		return done(null, false);
+	} catch (err) {
+		return done(err);
+	}
+});
 
-export default strategyJWT
+export default strategyJWT;

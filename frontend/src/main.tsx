e2dbec265559bfa3eab9790e9ea648f61e0b73b9
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-05-05 16:59:48 -0600

    added react-router with initial routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import App from "./App.tsx";
+import PostList from "./components/PostsList.tsx";
+import Post from "./components/Post.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [
+      {index:true, Component:PostList},
+      {path:'post/:postId', Component: Post}
+    ]
+  }
+])
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+   <RouterProvider router={router}>
+    </RouterProvider> 
+	</StrictMode>,
+);

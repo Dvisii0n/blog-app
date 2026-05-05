c3b1ac6cb0cfdc52c366139d21dbdf4474f914f1
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-05-05 17:03:57 -0600

    created featured post card variant

--- frontend/src/components/PostCard.tsx
+++ frontend/src/components/PostCard.tsx
@@ -1,17 +1,49 @@
-import type { PostData } from "../types/PostTypes"
-import { formatDate } from "../utils"
+import { useNavigate } from "react-router";
+import type { PostData } from "../types/PostTypes";
+import { formatDate } from "../utils";
 
-function PostCard({postData} : {postData:PostData }) {
-
-
-    return <div  className="post-card">
-        <p className="post-p created-at">{formatDate(postData.createdAt)}</p>
-        <p className="post-p author">Author: {postData.author.username}</p>
-        <p className="post-p title">{postData.title}</p>
-        <p className="post-p description">{postData.description}</p>
-        <button className="post-btn read">Read</button>
-    </div>
-
+function PostCard({ postData }: { postData: PostData }) {
+	const navigate = useNavigate();
+	return (
+		<div className="post-card">
+			<p className="post-p created-at">{formatDate(postData.createdAt)}</p>
+			<p className="post-p author">By: {postData.author.username}</p>
+			<p className="post-p title">{postData.title}</p>
+			<p className="post-p description">{postData.description}</p>
+			<button
+				onClick={() => navigate(`post/${postData.id}`)}
+				className="post-btn read"
+			>
+				Read
+			</button>
+		</div>
+	);
 }
 
-export default PostCard
+function PostCardFeatured({ postData }: { postData: PostData }) {
+	const navigate = useNavigate();
+	return (
+		<div className="post-card featured">
+			<img
+				className="post-img-featured"
+				src={postData.imageURL}
+				alt="featured post image"
+			/>
+			<p className="post-p created-at">
+				<span className="post-sp-featured">FEATURED</span>
+				{formatDate(postData.createdAt)}
+			</p>
+			<p className="post-p author">By: {postData.author.username}</p>
+			<p className="post-p title">{postData.title}</p>
+			<p className="post-p description">{postData.description}</p>
+			<button
+				onClick={() => navigate(`post/${postData.id}`)}
+				className="post-btn read"
+			>
+				Read
+			</button>
+		</div>
+	);
+}
+
+export { PostCard, PostCardFeatured };

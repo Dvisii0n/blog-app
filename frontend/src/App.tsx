ab849f349d1550722f9409e39ec05ece057865e2
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-05-07 20:15:35 -0600

    code reformat

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,15 @@
-import './styles.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import { Outlet } from 'react-router'
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router";
 function App() {
-
-  return (
-    <>
-    <Header></Header>
-    <Outlet/>
-    <Footer></Footer>
-    </>
-  )
+	return (
+		<>
+			<Header/> 
+      <Outlet />
+			<Footer/>
+		</>
+	);
 }
 
-export default App
+export default App;

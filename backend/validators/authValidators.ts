ff564bc453ac212b8d0d63d1d1917aa32a5861ba
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-05-09 18:22:52 -0600

    added confirm password validation

--- backend/validators/authValidators.ts
+++ backend/validators/authValidators.ts
@@ -1,10 +1,25 @@
+import { body } from "express-validator";
 import { userValidators } from "../validators/userValidators";
 import { validationErrorsHandler } from "./validationUtils";
+
+const validateConfirmPassword = [
+	body("confirmPassword")
+		.exists()
+		.withMessage(`Must input a password in confirm password`)
+		.trim()
+		.custom((value, { req }) => {
+			if (value !== req.body.password) {
+				throw new Error("Password confirmation does not match password");
+			}
+			return true;
+		}),
+];
 
 const validateSignUp = [
 	userValidators.validateEmail,
 	userValidators.validateUsername,
 	userValidators.validatePassword,
+	validateConfirmPassword,
 	validationErrorsHandler,
 ];
 

4c391e1861b0a18db9398319a0263c33b4b9a661
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-05-09 18:23:11 -0600

    changed min chars message

--- backend/validators/errorMessages.ts
+++ backend/validators/errorMessages.ts
@@ -1,7 +1,7 @@
 const errorMsg = {
 	exists: "is missing",
 	alphaNum: "must contain only letters and numbers",
-	minLength: "should contain more than",
+	minLength: "should contain at least",
 	maxLength: "should contain less than",
 	isEmail: "must be valid",
 };

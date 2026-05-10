725f031f26f9850a2faee2a9cbc1f8ac12ed3d30
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-05-09 18:21:06 -0600

    updated user icon src

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router";
+import { useNavigate } from "react-router";
 
 function Header() {
+	const navigate = useNavigate()
 	return (
 		<header>
 			<Link to={'/'} className="header-title">The Journal</Link>
 			<div className="header-right">
 				<button className="header-btn login">Log In</button>
-				<button className="header-btn signup">Sign Up</button>
-				<img src="/user.svg" alt="" className="header-icon user" />
+				<button onClick={() => navigate('signup')} className="header-btn signup">Sign Up</button>
+				<img src="/svg/user.svg" alt="" className="header-icon user" />
 			</div>
 		</header>
 	);

5b3595d6a491bc0d809081ad6033006e6da2b077
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-05-09 18:19:27 -0600

    finished signup page

--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -1,5 +1,12 @@
 import { useState, type ChangeEvent } from "react";
 import type { UserSignUp } from "../types/AuthTypes";
+import { Link } from "react-router";
+import { groupErrorsByField } from "../utils";
+import FormInput from "./FormInput";
+import { LineWobble } from "ldrs/react";
+import "ldrs/react/LineWobble.css";
+import { Navigate } from "react-router";
+
 
 function Signup() {
 	const signUpEndpoint = `${import.meta.env.VITE_API_URL}/auth/signup`;
@@ -7,10 +14,16 @@ function Signup() {
 		email: "",
 		username: "",
 		password: "",
+		confirmPassword: "",
 	});
-    const [errors, setErrors] = useState([])
+	const [errors, setErrors] = useState<Map<string, Array<string>> | null>(null);
+	const [loading, setLoading] = useState<Boolean>(false);
+	const [success, setSuccess] = useState<Boolean>(false);
 
-	function updateUserData(e: ChangeEvent<HTMLInputElement>, field: string) {
+	function updateUserData(
+		e: ChangeEvent<HTMLInputElement>,
+		field: string,
+	): void {
 		const obj: UserSignUp = userData;
 		obj[field as keyof typeof userData] = e.target.value;
 		setUserData(obj);
@@ -18,6 +31,7 @@ function Signup() {
 
 	async function handleSignUp() {
 		try {
+			setLoading(true);
 			const response = await fetch(signUpEndpoint, {
 				method: "POST",
 				headers: {
@@ -27,41 +41,80 @@ function Signup() {
 				body: JSON.stringify(userData),
 			});
 			const result = await response.json();
-            if (result.msg !== 'SIGNUP_SUCCESS') {
-                setErrors(result)
-            }
+			if (result[0].msg !== "SIGNUP_SUCCESS") {
+				const errors = groupErrorsByField(result);
+				setErrors(errors);
+			} else {
+				setSuccess(true);
+			}
 		} catch (error) {
 			throw error;
+		} finally {
+			setLoading(false);
 		}
 	}
 
-	return (
-		<main>
-			<form className="signup-form">
-				<label htmlFor="email">Email</label>
-				<input
-					type="email"
-					name="email"
-					onChange={(e) => updateUserData(e, "email")}
-					required
+	return success ? (
+		<Navigate to={"/login"} replace></Navigate>
+	) : (
+		<main className="user-form-page">
+			<form className="user-form signup-form">
+				<legend className="user-form-legend">Create an account</legend>
+				<section className="signup-top">
+					<FormInput
+						className={"form-input signup-email"}
+						labelTxt="Email"
+						name="email"
+						type="email"
+						onChange={updateUserData}
+						errors={errors}
+					/>
+					<FormInput
+						className={"form-input signup-username"}
+						labelTxt="Username"
+						name={"username"}
+						type={"text"}
+						onChange={updateUserData}
+						errors={errors}
+					/>
+				</section>
+				<FormInput
+					className={"form-input"}
+					labelTxt="Password"
+					name={"password"}
+					type={"password"}
+					onChange={updateUserData}
+					errors={errors}
 				/>
-				<label htmlFor="username">Username</label>
-				<input
-					type="text"
-					name="username"
-					onChange={(e) => updateUserData(e, "username")}
-					required
+				<FormInput
+					className={"form-input"}
+					labelTxt="Confirm Password"
+					name={"confirmPassword"}
+					type={"password"}
+					onChange={updateUserData}
+					errors={errors}
 				/>
-				<label htmlFor="password">Password</label>
-				<input
-					type="password"
-					name="password"
-					onChange={(e) => updateUserData(e, "password")}
-				/>
-				<button type="button" onClick={handleSignUp}>
+				<button type="button" className="submit-btn" onClick={handleSignUp}>
 					Sign Up
 				</button>
+				<section className="form-notice">
+					<span className="form-notice-sp">Already have an account? </span>
+					<Link className="form-anchor" to={""}>
+						Log In
+					</Link>
+				</section>
 			</form>
+			{loading ? (
+				<LineWobble
+					size="80"
+					stroke="5"
+					bgOpacity="0.1"
+					speed="1.75"
+					color="white"
+				/>
+			) : (
+				<></>
+			)}
 		</main>
 	);
 }

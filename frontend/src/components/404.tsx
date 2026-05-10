7dc526945cea736647ae7c5e595a69f5d01cdf46
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-05-09 18:21:27 -0600

    changed 400 page to 500 server error

--- frontend/src/components/404.tsx
+++
@@ -1,9 +0,0 @@
-function NotFound() {
-	return (
-		<main className="error-page">
-			<h1 className="not-found">Page Not Found :(</h1>
-		</main>
-	);
-}
-
-export default NotFound;
---
+++ frontend/src/components/500.tsx
@@ -0,0 +1,9 @@
+function ServerError() {
+	return (
+		<main className="error-page">
+			<h1 className="not-found">Something went wrong... :(</h1>
+		</main>
+	);
+}
+
+export default ServerError;
